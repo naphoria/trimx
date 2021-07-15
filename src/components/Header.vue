9fd4d746bf8800b8a71c907cf04ea677f7487dd5
nathanNMUK <nathan@northernmediauk.com>
2021-07-15 19:31:00 +0100

    missing details

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="bg-gray-800 shadow mb-2 border-b-4 border-blue-500" v-if="$route.meta.title == 'Home'">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold leading-tight text-white border-b-2 border-white inline-block pb-1">Barber Marketplace</h1>
+        <h1 class="text-3xl font-bold leading-tight text-white border-b-2 border-white inline-block pb-1">Marketplace</h1>
       </div>
     </header>
     <header class="bg-gray-800 shadow mb-2 border-b-4 border-blue-500" v-else-if="$route.meta.title">
